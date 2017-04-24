851e38fc9d8c4067bcba9001bfdc674c125c4460
Nathael Arki <nathael.arki@supinternet.fr>
2017-04-24 16:38:17 +0200

    nav bar edit profile page

--- assets/script/profileEditing.js
+++ assets/script/profileEditing.js
@@ -1,7 +1,5 @@
 $(function () {
 
-
-
     function emailValidation(email) {
         var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return emailRegExp.test(email);
