ae3c2437f8e83a72b3c7b6c1204479d1303a62ef
Nathael Arki <nathael.arki@supinternet.fr>
2017-04-26 12:26:54 +0200

    rename folder

--- Controllers/SecurityController.php
+++ Controllers/SecurityController.php
@@ -50,50 +50,6 @@ class SecurityController extends BaseController
         }
     }
 
-    public function profileAction()
-    {
-        if(isset($_GET['username'])){
-                $error = '';
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $manager = UserManager::getInstance();
-                if ($manager->userCheckArticles($_POST)) {
-                    $manager->insertArticles($_POST);
-                    //$this->redirect('profile');
-
-                } else {
-                    $error = 'invalid data';
-                }
-            }
-            $manager = UserManager::getInstance();
-            $user = $manager->getUserByUsername($_GET['username']);
-            if($user == false){
-                $this->redirect('home');
-            }
-            else{
-                if(isset($_SESSION['user_id'])){
-                    $isConnected = true;
-                    if($_SESSION['user_id'] == $user['id']){
-                        echo $this->renderView('profile.html.twig', ['error' => $error,
-                        'user' => $user, 'isConnected' => $isConnected]);
-                    }
-                    else{
-                        $visitor = $manager->getUserById($_SESSION['user_id']);
-                        echo $this->renderView('profile.html.twig', ['error' => $error,
-                            'user' => $user, 'isConnected' => $isConnected, 'visitor' => $visitor]);
-                    }
-                }
-                else{
-                    echo $this->renderView('profile.html.twig', ['error' => $error,
-                        'user' => $user]);
-                }
-            }
-        }
-        else{
-            $this->redirect('home');
-        }
-    }
-
-
     public function profileEditingAction()
     {
         $error = '';
